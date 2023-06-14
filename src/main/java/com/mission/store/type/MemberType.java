a930a11f7b0dc0885c03d627764e4c88a9765af9
jyjang <jinyngg@gmail.com>
2023-06-14 19:43:52 +0900

    feat : 매장 점주와 손님을 구분하기 위한 권한 타입 생성 (#9)

---
+++ src/main/java/com/mission/store/type/MemberType.java
@@ -0,0 +1,17 @@
+package com.mission.store.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberType {
+
+    OWNER("ROLE_OWNER", "매장 점주")
+    , CUSTOMER("ROLE_CUSTOMER", "매장 손님")
+
+    ;
+
+    private final String authority;
+    private final String description;
+}
