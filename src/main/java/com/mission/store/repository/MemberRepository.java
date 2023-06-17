c2fbb8bc8fcc21c69936d4c156d5822745ebcade
jyjang <jinyngg@gmail.com>
2023-06-17 23:54:11 +0900

    feat : 이메일 중복 체크 로직 추가 (#13)

--- src/main/java/com/mission/store/repository/MemberRepository.java
+++ src/main/java/com/mission/store/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.mission.store.repository;
 import com.mission.store.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, String> {
+
+    Optional<Member> findByEmail(String email);
 }
