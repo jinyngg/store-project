024325e5c6f8b685514d4d3b79ba0e63d42c1a97
jyjang <jinyngg@gmail.com>
2023-06-19 01:48:58 +0900

    docs : 이름 변경(MemberType -> MemberRole) (#16)
    1. 점주와 손님을 나타내기 위해 Type을 사용했지만, 권한 설정에 적합하지 않다고 판단되어 변경

--- src/main/java/com/mission/store/type/MemberRole.java
+++ src/main/java/com/mission/store/type/MemberRole.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum MemberType {
+public enum MemberRole {
 
     OWNER("ROLE_OWNER", "매장 점주")
     , CUSTOMER("ROLE_CUSTOMER", "매장 손님")
